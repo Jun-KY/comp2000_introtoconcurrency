23ca415d8ce258ab302e51a8d2d86951113d9f55
Damian Jurd <damian.jurd@mq.edu.au>
2021-10-21 17:54:10 +1100

    Updated date in copyright message. Added Dijkstra's head.

--- src/org/macquarie/head/Sprite.java
+++ src/org/macquarie/head/Sprite.java
@@ -2,23 +2,24 @@
  * This file is part of a project entitled IntroToConcurrency which is provided as
  * sample code for the following Macquarie University unit of study:
  * 
- * COMP229 "Object Oriented Programming Practices"
+ * COMP2000 "Object Oriented Programming Practices"
  * 
- * Copyright (c) 2011-2017 Dominic Verity and Macquarie University.
+ * Copyright (c) 2011-2021 Dominic Verity and Macquarie University.
+ * Copyright (c) 2011 The COMP229 Class.
  * 
- * IntroToConcurrency is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
+ * IntroToConcurrency is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  * 
- * IntroToConcurrency is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
+ * IntroToConcurrency is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with IntroToConcurrency. (See files COPYING and COPYING.LESSER.) If not,
- * see <http://www.gnu.org/licenses/>.
+ * along with IntroToConcurrency. (See files COPYING and COPYING.LESSER.) If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 package org.macquarie.head;
@@ -62,7 +63,7 @@ public class Sprite {
 	 */
 	public static Image getHead() {
 		if (mHead == null) {
-			mHead = new ImageIcon(Sprite.class.getResource("images/mediumhead.png")).getImage();
+			mHead = new ImageIcon(Sprite.class.getResource("images/dijkstra.jpg")).getImage();
 		}
 		return mHead;
 	}
--- src/org/macquarie/philosophy/Chopstick.java
+++ src/org/macquarie/philosophy/Chopstick.java
@@ -1,24 +1,25 @@
 /**
- * This file is part of a project entitled Week9Samples which is provided as
+ * This file is part of a project entitled IntroToConcurrency which is provided as
  * sample code for the following Macquarie University unit of study:
  * 
- * COMP229 "Object Oriented Programming Practices"
+ * COMP2000 "Object Oriented Programming Practices"
  * 
- * Copyright (c) 2011-2012 Dominic Verity and Macquarie University.
+ * Copyright (c) 2011-2021 Dominic Verity and Macquarie University.
+ * Copyright (c) 2011 The COMP229 Class.
  * 
- * Week9Samples is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
+ * IntroToConcurrency is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  * 
- * Week9Samples is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
+ * IntroToConcurrency is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Week9Samples. (See files COPYING and COPYING.LESSER.) If not,
- * see <http://www.gnu.org/licenses/>.
+ * along with IntroToConcurrency. (See files COPYING and COPYING.LESSER.) If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 package org.macquarie.philosophy;
